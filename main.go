f0a58fe1357b4e8f783e33f3976022a06dea185f
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-03-05 18:16:49 -0300

    first commit with net/http instead of fiber go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+  "html/template"
+ // "time"
+  "log"
+  "net/http"
+ // "github.com/gofiber/fiber/v2"
+)
+
+func main(){
+  fmt.Println("Hello web.")
+
+  h1 := func(w http.ResponseWriter, r *http.Request){
+    templ := template.Must(template.ParseFiles("index.html"))
+    templ.Execute(w, nil)
+  }
+
+ 
+  http.HandleFunc("/", h1)
+  log.Fatal(http.ListenAndServe(":8000", nil))
+}
