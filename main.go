019a0f0960e9cafd22a3eab730a55242dc2baaf6
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-04-17 10:04:37 -0300

    setup para MongoDB e .env

--- main.go
+++ main.go
@@ -10,26 +10,50 @@ import (
 )
 
 func main() {
-    app := fiber.New(fiber.Config{
-      Views: html.New("./views", ".html"),
-    })
 
-    app.Static("/", "./public", fiber.Static{
-      Compress: true,
-    })
+  //Conectando ao MongoDB com MongoDriver para Go.
+  clientOptions := options.Client().ApplyURI("")
+  
+  //Context mongo.
+  ctx := context.Background()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-      return c.Render("index", fiber.Map{})
-    })
+  //Conectando ao cliente mongo.
+  client, err := mongo.Connect(ctx, clientOptions)
+  if err != nil {
+    log.Fatal(err)
+  }
 
-    app.Get("/about", func(c *fiber.Ctx) error {
-      return c.Render("about", fiber.Map{})
-    })
+  //Verificando a conexão com o banco de dados
+  err = client.Ping(ctx, nil)
+  if err != nil {
+    log.Fatal(err)
+  }
 
-    app.Get("/hackerman", func(c *fiber.Ctx) error {
-      return c.Render("hackerman", fiber.Map{})
-    })
+  fmt.Println("Conectado ao MongoDB.")
 
-    app.Listen(":4000")
+  //Definindo database e collection
+  collection := client.Database("").Collection("") 
+
+  app := fiber.New(fiber.Config{
+    Views: html.New("./views", ".html"),
+  })
+
+  app.Static("/", "./public", fiber.Static{
+    Compress: true,
+  })
+
+  app.Get("/", func(c *fiber.Ctx) error {
+    return c.Render("index", fiber.Map{})
+  })
+
+  app.Get("/about", func(c *fiber.Ctx) error {
+    return c.Render("about", fiber.Map{})
+  })
+
+  app.Get("/hackerman", func(c *fiber.Ctx) error {
+    return c.Render("hackerman", fiber.Map{})
+  })
+
+  app.Listen(":4000")
 }
 //bloating go text.
