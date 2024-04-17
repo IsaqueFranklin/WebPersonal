6affae24e22e4e30ba0e5101e3ea236a9a33dc44
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-04-17 10:17:05 -0300

    connecting to mongo and getting .env

--- main.go
+++ main.go
@@ -4,15 +4,28 @@ import (
   //"fmt"
   //"log" 
   //"html/template"
+  "github.com/joho/godotenv"
   "github.com/gofiber/template/html/v2"
   "github.com/gofiber/fiber/v2"
-  //"github.com/gofiber/template/html/v2"
+  "go.mongodb.org/mongo-driver/bson"
+  "go.mongodb.org/mongo-driver/mongo"
+  "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+  
+  //Pegando URI do arquivo .env
+  if err := godotenv.Load(); err != nil {
+		log.Println("Não encontrado aquivo .env no sistema.")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("Você precisa criar sua variável 'MONGODB_URI' no arquivo .env.")
+	}
 
   //Conectando ao MongoDB com MongoDriver para Go.
-  clientOptions := options.Client().ApplyURI("")
+  clientOptions := options.Client().ApplyURI(uri)
   
   //Context mongo.
   ctx := context.Background()
